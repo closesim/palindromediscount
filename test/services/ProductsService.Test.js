f7d9138658bb50eb3da60db9564a4c61cfe4211a
Miguel Borja <borjainside@gmail.com>
2020-07-28 17:02:18 -0500

    [DEV] Actualizado opciones linter

--- test/services/ProductsService.Test.js
+++ test/services/ProductsService.Test.js
@@ -1,11 +1,9 @@
-// const assert = require('assert');
-
 const assert = require('assert');
 const { checkProdsForPal } = require('../../services/Products.service');
 const ProductsTestData = require('./test_data/ProductsTestData');
 
 describe('Palindrome Test Cases', () => {
-  it.only('Should be true for abba', () => {
+  it('Should be true for abba', () => {
     const products = checkProdsForPal(ProductsTestData.products);
     assert.equal(products.length, 2);
     products.forEach((product) => {
